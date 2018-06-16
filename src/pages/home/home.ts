52b1964713f35b28519014eb0b76df78b29a7b1a
Andre Lopes <andrevitorlopes@gmail.com>
2018-06-16 20:39:32 -0300

    preparando chart js e adicionando variáveis para uso futuro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireList, AngularFireObject } from 'angularfire2/database/interfaces';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  data: Observable<any[]>;
+  ref: AngularFireList<any>;
+  unidades = [
+    { value: 'unidade1', name: 'Unidade ES'},
+    { value: 'unidade2', name: 'Unidade SP'},
+    { value: 'unidade3', name: 'Unidade RS'},
+    { value: 'unidade4', name: 'Unidade RJ'}
+  ];
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('graficoCanvas1') graficoCanvas1;
+  @ViewChild('graficoCanvas2') graficoCanvas2;
+
+  objChartJs: any;
+  objChartJs2: any;
+
+  chartData = null;
+
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase, public loadingCtrl: LoadingController) {
 
   }
 
